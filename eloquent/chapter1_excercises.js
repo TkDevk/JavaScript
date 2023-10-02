ad374505b20b4679c72892b1602535061116a1ef
TkDevk <duvanuriel@hotmail.com>
2023-10-01 22:24:32 -0500

    actually is chapter 2

--- eloquent/chapter1_excercises.js
+++ eloquent/chapter1_excercises.js
@@ -73,8 +73,8 @@ console.log(`---------------------------------------CHESSBOARD------------------
  */
 //let square = " #";
 //let blank =" ";
-for(i=0; i<4; i++){
-       console.log(" # # # #\n# # # # ")
+for (i = 0; i < 4; i++) {
+    console.log(" # # # #\n# # # # ")
 }
 
 
@@ -83,7 +83,49 @@ for(i=0; i<4; i++){
  * When you have a program that generates this pattern, define a binding size=8 and change the program so that it works for any size, outputting a grid of the given width and height. 
  * 
  */
+/*
+let inLine = 2; // # #
+let outLine = 2; // rows
 
+
+if (inLine  % 2 == 0) {
+  let blank = ""
+  let hash="#"
+    for (let i = inLine; i < outLine; i++){
+        hash = hash + blank
+        console.log(hash)
+    }
+   
+
+     
+} else {
+    console.log("only pares pa")
+}
+
+
+*/
+
+console.log("----------------------------OverStack Solution for chessBoard excercise---------------------------")
+
+//I need two loops, the bigger one is for Y that will be my rows and the inner will be for the X's this on it will be place by place, the hint to place "" before "#" it's using the conditional with modular ((x+y)%2==0), why? because of each iteration of X i will calculate if my number is par, if not i will place "#""
+
+
+let size = 8;
+let position = "";
+
+for (let y = 0; y < size; y++) {  //Rows loop
+    for (let x = 0; x < size; x++) { //Column loop
+        if ((x + y) % 2 == 0) {
+            position = position + " "; //If %2== 0, so place a " "
+        } else {
+            position = position + "#"; // if not place a "#"
+        }
+
+    }
+
+    position = position + "\n"; //When the loop it's finished i need to break the line so
+}
+console.log(position)
 
 /*
 function test(x1,x2){
